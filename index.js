12f1b59ab846733942672ee61c14803ae3a1764f
hwg <wghuangzj@sina.com>
2022-05-12 17:05:22 +0800

    2

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @author hwg
+ */
+
+const projectPath = '' // 项目绝对路径
+const findList = [] // 需要处理的文件后缀，参考以下方案
+// const findList = ['*.png', '*.jpg', '*.gif']
+// const findList = ['*.mp3', '*.mp4', '*.avi', '*.mov']
